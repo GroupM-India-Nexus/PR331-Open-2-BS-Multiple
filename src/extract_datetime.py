d16cd3e7f18454693e1a8bbccc293b9be7eb6771
Sachingroupm10 <sachin.saurav@groupm.com>
2025-09-28 15:00:27 +0530

    Delete src directory

--- src/extract_datetime.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def extract_start_end_time(program_str):
-    match = re.search(r'\d{2}\.\d{2}', program_str)
-    if match:
-        times = re.findall(r'\d{2}\.\d{2}', program_str)
-        if len(times) == 2:
-            return times[0], times[1]
-    return "07.00", "24.00" # Default values if no match is found    
